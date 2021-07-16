5893a8e2bf115f9d25b243700d6eccb7d38664f3
Mitt Shah <mittshah2@gmail.com>
2021-07-16 21:22:24 +0530

    batch norm

--- detectron2/layers/blocks.py
+++ detectron2/layers/blocks.py
@@ -4,7 +4,7 @@
 import fvcore.nn.weight_init as weight_init
 from torch import nn
 
-from .batch_norm import FrozenBatchNorm2d, get_norm
+from .batch_norm import get_norm
 from .wrappers import Conv2d
 
 
